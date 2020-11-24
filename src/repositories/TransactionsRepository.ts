b9278986c5d987196fb4d97758f9eecf0ac73ba1
Maiquel <maiquelp@gmail.com>
2020-11-23 21:22:27 -0300

    :boom: finish

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,41 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+    // const {income, outcome} = transactions.reduce(
+    //   (accumulator, transaction) => {
+    //     switch (transaction.type) {
+    //       case 'income':
+    //         accumulator.income += Number(transaction.value);
+    //         break;
+    //       case 'outcome':
+    //         accumulator.outcome += Number(transaction.value);
+    //         break;
+    //       default: 
+    //         break;
+    //     }
+    //     return accumulator;
+    //   }, {
+    //     income: 0,
+    //     outcome: 0,
+    //     total: 0,
+    //   },
+    // );
+    // const total = income - outcome;
+    // return {income, outcome, total};
+
+    const balance: Balance = { income: 0, outcome: 0, total: 0 } 
+    if (transactions) {
+      transactions.map( element => {
+        if (element.type === 'income') {
+          balance.income += Number(element.value);
+        }else if (element.type === 'outcome'){
+          balance.outcome += Number(element.value);
+        }
+      });
+    };
+    balance.total = balance.income - balance.outcome;
+    return balance
   }
 }
 
--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,26 +1,59 @@
 import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
+import multer from 'multer';
+import uploadConfig from '../configs/upload';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
-// import DeleteTransactionService from '../services/DeleteTransactionService';
-// import ImportTransactionsService from '../services/ImportTransactionsService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
+import DeleteTransactionService from '../services/DeleteTransactionService';
+import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
 
+const upload = multer(uploadConfig);
+
 transactionsRouter.get('/', async (request, response) => {
-  // TODO
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const transactions = await transactionsRepository.find(); 
+    const balance = await transactionsRepository.getBalance();
+    const consolidation = { transactions, balance }
+    return response.json(consolidation);
 });
 
 transactionsRouter.post('/', async (request, response) => {
-  // TODO
+  try {
+    const {title, value, type, category} = request.body;
+    const createTransaction = new CreateTransactionService();
+    const transaction = await createTransaction.execute({title, value, type, category})
+    return response.json(transaction);
+  } catch (error) {
+    return response.status(400).json({message: error.message, status: 'error'});
+  }
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
-  // TODO
+  try {
+    const {id} = request.params;
+    const deleteTransaction = new DeleteTransactionService();
+    await deleteTransaction.execute(id);
+    return response.status(204).send();
+    
+  } catch (error) {
+    return response.status(400).json({message: error.message, status: 'error'});
+  }
+
 });
 
-transactionsRouter.post('/import', async (request, response) => {
-  // TODO
+transactionsRouter.post('/import', upload.single('file'), async (request, response) => {
+  try {
+    const {filename, destination} = request.file;
+    const importTransaction = new ImportTransactionsService();
+    const transaction = await importTransaction.execute(filename, destination);
+    return response.json(transaction);
+  } catch (error) {
+    return response.status(400).json({message: error.message, status: 'error'});
+  }
+  
 });
 
 export default transactionsRouter;
